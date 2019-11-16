06c7af597c2dfaab533e84f7ad1c38aa0e07db26
yannclaes <yannclaes@msn.com>
2019-11-16 16:01:27 +0100

    Addition of histogram plots

--- 2/R/robust_detection.R
+++ 2/R/robust_detection.R
@@ -3,6 +3,7 @@ library(MASS)
 library(reshape2)
 library(ggplot2)
 library(hexbin)
+library(dplyr)
 
 # ---------- PART 3.1 : Robust Outlier Detection ----------
 data <- read.table("products/csv/data.csv", header = TRUE, na.strings = NA, sep = ",")
@@ -44,6 +45,47 @@ plt <- plt + geom_hline(yintercept = qchisq(0.95, dim(quantitative_data)[2]), co
 plt <- plt + labs(x = "Classic Mahalanobis distances", y = "Robust Mahalanobis distances")
 ggsave(filename = "products/pdf/compare_maha_most_robust.pdf")
 
+# Compute outlying rates for both methods
 rob_outliers_rate <- sum(robust_maha > qchisq(.95, dim(quantitative_data)[2]))/dim(data)[1]
 classical_outliers_rate <- sum(classic_maha > qchisq(.95, dim(quantitative_data)[2]))/dim(data)[1]
 
+# Retrieve outlying observations for both methods
+outlying_rob_idx <- robust_maha > qchisq(.95, dim(quantitative_data)[2])
+outlying_classic_idx <- classic_maha > qchisq(.95, dim(quantitative_data)[2])
+
+outlying_obs_rob <- quantitative_data[outlying_rob_idx, ]
+outlying_obs_classic <- quantitative_data[outlying_classic_idx, ] 
+
+non_outlying <- anti_join(quantitative_data, outlying_obs_rob)
+common_outliers <- inner_join(outlying_obs_classic, outlying_obs_rob)
+only_robust_outliers <- anti_join(outlying_obs_rob, outlying_obs_classic)
+
+# Plot histograms of quantitative data (outlying and non outlying)
+melted <- melt(quantitative_data)
+plt <- ggplot(melted, aes(x = value)) + geom_histogram()
+plt <- plt + facet_wrap( ~ variable, scales = "free") + labs(x = "", y = "")
+ggsave("products/pdf/histograms.pdf", plt)
+
+# Plot histograms of quantitative data (non outlying)
+melted <- melt(non_outlying)
+plt <- ggplot(melted, aes(x = value)) + geom_histogram()
+plt <- plt + facet_wrap( ~ variable, scales = "free") + labs(x = "", y = "")
+ggsave("products/pdf/histograms_non_outlying.pdf", plt)
+
+# Plot histograms of common outlying observations
+melted <- melt(common_outliers)
+plt <- ggplot(melted, aes(x = value)) + geom_histogram()
+plt <- plt + facet_wrap( ~ variable, scales = "free") + labs(x = "", y = "")
+ggsave("products/pdf/histograms_common_outliers.pdf", plt)
+
+# Plot histograms of robust outlying observations not detected by the classical approach
+melted <- melt(only_robust_outliers)
+plt <- ggplot(melted, aes(x = value)) + geom_histogram()
+plt <- plt + facet_wrap( ~ variable, scales = "free") + labs(x = "", y = "")
+ggsave("products/pdf/histograms_robust_only.pdf", plt)
+
+# Plot histograms of all robust outlying observations
+melted <- melt(outlying_obs_rob)
+plt <- ggplot(melted, aes(x = value)) + geom_histogram()
+plt <- plt + facet_wrap( ~ variable, scales = "free") + labs(x = "", y = "")
+ggsave("products/pdf/histograms_robust_all.pdf", plt)
