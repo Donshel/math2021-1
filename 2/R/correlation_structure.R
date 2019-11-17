4efd76df7a0b19d911420803bba02edbef7e8c85
François Rozet <francois.rozet@outlook.com>
2019-11-17 17:50:45 +0100

    L1-regularisation correction (correlation_structure.R)

--- 2/R/correlation_structure.R
+++ 2/R/correlation_structure.R
@@ -1,4 +1,6 @@
 # Libraries
+library(corrplot)
+library(ggplot2)
 library(MASS)
 library(qgraph)
 library(huge)
@@ -7,8 +9,6 @@ library(huge)
 data <- read.table("products/csv/data.csv", header = TRUE, na.strings = NA, sep = ",")
 data <- data[complete.cases(data), ]
 attach(data)
-n <- dim(data)[1]
-d <- dim(data)[2]
 
 dir.create("products", showWarnings = FALSE)
 dir.create("products/pdf", showWarnings = FALSE)
@@ -16,38 +16,50 @@ dir.create("products/pdf", showWarnings = FALSE)
 quantitative_cols <- c("PM2.5", "PM10", "SO2", "NO2", "CO", "O3", "temp", "pres", "dewp", "wspd")
 quantitative_data <- data[quantitative_cols]
 
-## Compute robust estimation of correlation matrix using MCD estimator (Coverage parameter of 0.75)
-## on quantitative variables and compare with classical correlation matrix (do not take time variables
-## into account).
-classic_corr <- cor(quantitative_data)
-
-COVERAGE <- 0.75
+## 1. Robust correlation matrix
 set.seed(0)
-robust <- cov.rob(quantitative_data, cor = TRUE, quantile.used = COVERAGE * dim(data)[1], method = "mcd")
-robust_corr <- robust$cor
+h <- floor(0.75 * dim(data)[1])
+robust <- cov.rob(quantitative_data, cor = TRUE, quantile.used = h, method = "mcd")
 
+pdf("products/pdf/classic_correlation.pdf")
+corrplot(cor(quantitative_data), method = "color", type = "lower", tl.col = "black", tl.pos = "ld", tl.srt = 45)
+dev.off()
 
-## Graphical model to visualize conditional independence (set fade to FALSE in order to see all edges)
-# Use classic covariance matrix
+pdf("products/pdf/robust_correlation.pdf")
+corrplot(robust$cor, method = "color", type = "lower", tl.col = "black", tl.pos = "ld", tl.srt = 45)
+dev.off()
+
+## 2. Graphical models
+
+### 2.a Classic covariance
 classic_cov <- cov(quantitative_data)
-qgraph(classic_cov, fade = FALSE, edge.labels = TRUE, filetype = "pdf", filename = "products/pdf/conditional_dep_S")
+qgraph(classic_cov, fade = FALSE, edge.labels = TRUE, filetype = "pdf", filename = "products/pdf/qgraph_classic_cov")
 
-# Choose optimal lambda for L1 regularization with BIC method
-seqlambda <- seq(0,1, length.out=10)
-BIC <- NULL
-for(lambda in seqlambda)
-{
-  l1reg <- huge(classic_cov, lambda, method="glasso", cov.output= TRUE)
-  l1prec <- solve(as.matrix(l1reg$cov[[1]])) ; l1loglik <- l1reg$loglik * n/2
-  BIC <- c(BIC, -2*l1loglik +
-             (d + sum(upper.tri(l1prec, diag=TRUE) != 0) * log(n)))
+### 2.b L1-regularized covariance
+
+#### Optimal lambda using BIC method
+lambda <- seq(0, 1, length.out = 10)
+BIC <- rep(0, 10)
+
+n <- dim(quantitative_data)[1]
+p <- dim(quantitative_data)[2]
+
+for (i in 1:length(lambda)) {
+	l1reg <- huge(classic_cov, lambda[i], method = "glasso", cov.output = TRUE)
+	l1prec <- solve(as.matrix(l1reg$cov[[1]]))
+	BIC[i] <- -n * l1reg$loglik +
+		log(n) * (p + sum(l1prec[upper.tri(l1prec, diag = TRUE)] != 0))
 }
-lambda <- seqlambda[which.min(BIC)]
 
-# Perfom L1 regularization with optimal lambda and plot resulting graph
-# Only slight changes can be noticed in the edges' weights.
-l1reg <-  huge(classic_cov, lambda, method = "glasso", cov.output = TRUE)
-reg_cov <- l1reg$cov[[1]]
-rownames(reg_cov) <- rownames(classic_cov)
-colnames(reg_cov) <- colnames(classic_cov)
-qgraph(reg_cov, fade = FALSE, edge.labels = TRUE, filetype = "pdf", filename = "products/pdf/conditional_dep_regu")
+plt <- ggplot() + geom_line(aes(x = lambda, y = BIC))
+ggsave(filename = "products/pdf/optimal_lambda.pdf", plt)
+
+lambda <- lambda[which.min(BIC)]
+
+#### L1-regularization with optimal lambda
+l1reg <- huge(classic_cov, lambda, method = "glasso", cov.output = TRUE)
+l1_cov <- l1reg$cov[[1]]
+rownames(l1_cov) <- rownames(classic_cov)
+colnames(l1_cov) <- colnames(classic_cov)
+
+qgraph(l1_cov, fade = FALSE, edge.labels = TRUE, filetype = "pdf", filename = "products/pdf/qgraph_l1_cov")
